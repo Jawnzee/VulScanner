4f65c569faa1f05c81ba36b2bb6456de4d752b0b
Jawnzee <jonathanperez.cyber@gmail.com>
2024-08-28 02:39:51 -0500

    Testing Commit and Sync

---
+++ src/vulscanner.py
@@ -0,0 +1,18 @@
+"""
+VulScanner - A Vulnerability Scanning Tool
+Author: Jawnzee
+Date: August 28, 2024
+Version: 1.0
+
+Description:
+VulScanner is designed to identify active hosts, open ports, and associated vulnerabilities 
+within a specified IP range. This tool integrates with the CVE database to provide detailed 
+vulnerability information based on the services detected on open ports.
+
+Dependencies:
+- Python 3.x
+- Scapy
+- Requests
+- nvdlib
+"""
+
